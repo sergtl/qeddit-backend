1b17bc8e2828b35a9f78956c3f7d08cf6f6087a8
Sergei Topal <sergeitopal98@gmail.com>
2024-06-04 17:18:15 +0300

    add db models and some crud endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [PostsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
